1421f3f31238d687e35277bca26fccd870e44714
Jeff Hemminger <jeff.hemminger@deciphernow.com>
2019-04-29 08:16:07 -0400

    handling custom entries

--- init.el
+++ init.el
@@ -40,17 +40,3 @@
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
 (setq gc-cons-threshold 800000)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-tomato forge magit-popup magit-gh-pulls flycheck go-complete go-eldoc go-mode auto-complete ensime nord-theme kosmos-theme madhat2r-theme sublime-themes subatomic-theme soothe-theme plan9-theme heroku-theme github-theme espresso-theme cyberpunk-theme bliss-theme birds-of-paradise-plus-theme atom-one-dark-theme arjen-grey-theme white-theme punpun-theme idea-darkula-theme doom-themes neotree counsel-projectile counsel flx ivy yaml-mode terraform-mode exec-path-from-shell golden-ratio expand-region org-bullets restclient autopair projectile magit use-package paradox))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
